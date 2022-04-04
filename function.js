9d067052a42b9d2e11ecbb14ca7f33862c6bddc2
DINESHBABU1998 <98023477+DINESHBABU1998@users.noreply.github.com>
2022-04-04 20:39:04 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,61 @@
+//Create one function with zero parameter having a console statement;
+// function sayHi(){
+//    console.log("Hi There");   //output is Hi There
+// }
+// sayHi()
+
+//Create one function which takes two values as a parameter and print the sum of them as "Sum of 3, 4 is 7"
+// function sum(a,b){
+//    console.log(a+b);   //output is 7
+// }
+// sum(3,4);
+
+//Create one arrow function
+// let sum=(a,b)=>{
+// console.log(a+b);      //output is 7
+// }
+// sum(3,4)
+
+//Print output: 
+// var x = 21;
+// var girl = function () {
+//     console.log(x);
+//     var x = 20;
+// };
+// girl ();   //output is undefined
+
+//Print output: 
+// var x = 21;
+// girl ();
+// console.log(x)   
+// function girl() {
+//     console.log(x);
+//     var x = 20;
+// };           //output is undefined and 21
+
+//Print output
+// var x = 21;
+// console.log(a); //a is not defined---reference error
+// a();
+// b();
+// a = function() {
+    
+//    x = 20;
+//   console.log(x);  
+// };
+// b = function() {
+    
+//     x = 40;
+//    console.log(x); 
+// };
+
+//Write a function that accepts parameter n and returns factorial of n
+// function factorial(n) {
+//    if (n == 0 || n == 1) {
+//        return 1;
+//    } else {
+//        return n * factorial(n - 1);
+//    }
+// }
+// let answer = factorial(5)
+// console.log(answer);
